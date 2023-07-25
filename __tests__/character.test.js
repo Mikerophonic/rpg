5495c420316d802f3bcdad237bf9ec7d6eee4c78
Mikerophonic <micgcon@gmail.com>
2023-07-24 19:47:22 -0700

    pass 2nd test making character type sub-class

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,14 +1,27 @@
-import Character from './../src/character.js';
+import {Character, Warrior} from './../src/character.js';
     describe ('Character', () => {
         let character;
         beforeEach(() => {
-            character = new Character(12, 12, 13, 23, 43);
+            character = new Character();
         });
         test('It should create a new character object', () => {
+            character = new Character(12, 12, 13, 23, 43);
             expect(character.str).toEqual(12);
             expect(character.dex).toEqual(12);
             expect(character.int).toEqual(13);
             expect(character.def).toEqual(23);
             expect(character.mag).toEqual(43);
-        })
+        });
+        test('It should create a warrior class with its own starting stats and special ability', () => {
+            let warrior = new Warrior;
+            expect(warrior.str).toEqual(50);
+            expect(warrior.dex).toEqual(35);
+            expect(warrior.int).toEqual(0);
+            expect(warrior.def).toEqual(40);
+            expect(warrior.mag).toEqual(2);
+        });
+        test('It should create a name for the character', () => {
+            let warrior = new Warrior;
+            expect(warrior.name).toEqual("Conan");
+        });
     });
--- src/character.js
+++ src/character.js
@@ -1,11 +1,27 @@
-export default class Character {
-    constructor(str, dex, int, def, mag) {
+export class Character {
+    constructor(str, dex, int, def, mag, name) {
       this.str = str;
       this.dex = dex;
       this.int = int;
       this.def = def;
       this.mag = mag;
+      this.name = name;
     }
-  
     
   }
+
+export class Warrior extends Character {
+    constructor(str, dex, int, def, mag, name) {
+    super(str, dex, int, def, mag, name); 
+    this.str = 50;
+    this.dex = 35;
+    this.int = 0;
+    this.def = 40;
+    this.mag = 2;
+    this.name = "Conan";
+  }
+
+    bigThrust() {
+    console.log(this.name + " thrusted bigly!")
+    }
+}
